4fd985d57a85b3ee0256e28b25340ea60a788ce2
Aditya Kumar Sharma <vishwaaditya2003@gmail.com>
2022-05-06 23:02:06 +0530

    Create main.h

---
+++ main.h
@@ -0,0 +1,23 @@
+#define ADMINUSER "admin"
+#define ADMINPASSWORD "Kamakhaya"
+
+#define USER "user"
+#define USERPASSWORD "1234"
+
+#define FINE 500
+
+int addBook();
+
+int deleteBook();
+
+int updateBook();
+
+int listOfAllBooks();
+
+int borrowBook();
+
+int returnBook();
+
+int adminLogin();
+
+int userLogin();
